8382d3f133aef22e78cc4de351565b58634f365b
Dima123kr <krivosheya_da@mail.ru>
2022-02-10 21:10:47 +0300

    Космические Бои 0.5
    Небольшие изменения

--- hello/main.lua
+++ hello/main.lua
@@ -1,4 +1,5 @@
 local hc = require "lib/hc"
+local timer = require "lib/timer"
 x = 50
 y = 50
 r = 0
@@ -14,6 +15,8 @@ p6 = 0
 p7 = 0
 p8 = 0
 p9 = 0
+p10 = 0
+p11 = 0
 pulak = {}
 pula1 = {}
 pula = {}
@@ -22,6 +25,8 @@ ty2 = {}
 tr2 = {}
 erx = {}
 ery = {}
+ix2 = {}
+iy2 = {}
 tx3 = {}
 ty3 = {}
 tr3 = {}
@@ -51,7 +56,7 @@ function pula_load(r2, name)
 	end
 	p4 = p4 + 1
 end
-function loser_load(x3, y3, r3)
+function loser_load(r3)
 	tx3[p7] = 1000
 	ty3[p7] = 1000
 	tr3[p7] = r3
@@ -60,14 +65,16 @@ function loser_load(x3, y3, r3)
 	loser[p7] = hc.register(loser1[p7])
 	loser[p7]:setRotation(r3)
 end
-function pula_update(vx, vy, x2, y2, ox2, oy2)
-	ix2 = x2 + ox2
-	iy2 = y2 + oy2
-	p1, p2, p3 = ship:collidesWith(pula[p5])
-	if p1 == true or tx2[p5] < -100 or tx2[p5] > 900 or ty2[p5] < -100 or ty2[p5] > 700 then
-		tx2[p5] = ix2 + erx[p5]
-		ty2[p5] = iy2 + ery[p5]
-		pula[p5]:moveTo(ix2, iy2)
+function pula_update(vx, vy, x2, y2, ox2, oy2, v2)
+	ix2[p5] = x2 + ox2
+	iy2[p5] = y2 + oy2
+	if v2 == 1 then
+		p1, p2, p3 = ship:collidesWith(pula[p5])
+	end
+	if p1 == true then
+		tx2[p5] = 1000
+		ty2[p5] = 1000
+		pula[p5]:moveTo(1000, 1000)
 	end
 	tx2[p5] = tx2[p5] + vx * love.timer.getDelta()
 	ty2[p5] = ty2[p5] + vy * love.timer.getDelta()
@@ -77,8 +84,37 @@ function pula_update(vx, vy, x2, y2, ox2, oy2)
 		p5 = 0
 	end
 end
-function loser_update(v, x3, y3)
-	
+function loser_update(v3, x3, y3)
+	timer.after(3, function() p10 = 1 end)
+	if p10 == 1 then
+		tx2[p8] = ix2[p8] + erx[p8]
+		ty2[p8] = iy2[p8] + ery[p8]
+		pula[p8]:moveTo(ix2[p8], iy2[p8])
+		p8 = p8 + 1
+		tx2[p8] = ix2[p8] + erx[p8]
+		ty2[p8] = iy2[p8] + ery[p8]
+		pula[p8]:moveTo(ix2[p8], iy2[p8])
+		p8 = p8 + 1
+		tx2[p8] = ix2[p8] + erx[p8]
+		ty2[p8] = iy2[p8] + ery[p8]
+		pula[p8]:moveTo(ix2[p8], iy2[p8])
+		p8 = p8 + 1
+		tx2[p8] = ix2[p8] + erx[p8]
+		ty2[p8] = iy2[p8] + ery[p8]
+		pula[p8]:moveTo(ix2[p8], iy2[p8])
+		p8 = p8 + 1
+	end
+	tx3[p11] = x3
+	ty3[p11] = y3
+	loser[p11]:moveTo(tx3[p11],ty3[p11] - 185)
+	p11 = p11 + 1
+	if p11 == 1 then
+		p11 = 0
+	end
+	if p8 == 24 then
+		p8 = 0
+	end
+	p10 = 0
 end
 function pula_draw()
 	love.graphics.draw(pulak[p6], tx2[p6], ty2[p6], tr2[p6], z2, z2)
@@ -89,11 +125,15 @@ function pula_draw()
 	end
 end
 function loser_draw()
-	
+	love.graphics.draw(loserk[p9], x3, y3, r3, z3, z3, 640, 135)
+	loser[p9]:draw("fill")
+	p9 = p9 + 1
+	if p9 == 1 then
+		p9 = 0
+	end
 end
 function love.load()
 	player = love.graphics.newImage("res/ship.png")
-	loser11 = love.graphics.newImage("res/loser1.jpg")
 	ship1 = hc.polygon(0,65*z, 35*z,65*z, 35*z,225*z, 140*z,225*z, 140*z,0, 215*z,0, 215*z,225*z, 320*z,225*z, 320*z,65*z, 355*z,65*z, 355*z,355*z, 0,355*z)
 	ship = hc.register(ship1)
 	ship:moveTo(x, y)
@@ -154,8 +194,10 @@ function love.load()
 	pula_load(-math.pi / 2,"res/pula2.png")
 	pula_load(-math.pi / 2,"res/pula2.png")
 	---------------------------------
+	loser_load(0)
 end
 function love.update(dt)
+	timer.update(dt)
 	if love.keyboard.isDown("w") and y >= 0 then
 		y = y - 100 * dt
 		r = 0
@@ -182,40 +224,40 @@ function love.update(dt)
 		ship:move(100 * dt, 0)
 	end
 	-------------------------------------
-	pula_update(-100, 0, x3, y3, -50, 20)
-	pula_update(-100, 0, x3, y3, -50, 20)
-	pula_update(-100, 0, x3, y3, -50, 20)
-	pula_update(-100, 0, x3, y3, -50, 20)
-	pula_update(-100, 0, x3, y3, -50, 20)
-	pula_update(-100, 0, x3, y3, -50, 20)
+	pula_update(-100, 0, x3, y3, -50, 20,1)
+	pula_update(-100, 0, x3, y3, -50, 20,1)
+	pula_update(-100, 0, x3, y3, -50, 20,1)
+	pula_update(-100, 0, x3, y3, -50, 20,1)
+	pula_update(-100, 0, x3, y3, -50, 20,1)
+	pula_update(-100, 0, x3, y3, -50, 20,1)
 	-------------------------------------
-	pula_update(100, 0, x3, y3, 50, 20)
-	pula_update(100, 0, x3, y3, 50, 20)
-	pula_update(100, 0, x3, y3, 50, 20)
-	pula_update(100, 0, x3, y3, 50, 20)
-	pula_update(100, 0, x3, y3, 50, 20)
-	pula_update(100, 0, x3, y3, 50, 20)
+	pula_update(100, 0, x3, y3, 50, 20,1)
+	pula_update(100, 0, x3, y3, 50, 20,1)
+	pula_update(100, 0, x3, y3, 50, 20,1)
+	pula_update(100, 0, x3, y3, 50, 20,1)
+	pula_update(100, 0, x3, y3, 50, 20,1)
+	pula_update(100, 0, x3, y3, 50, 20,1)
 	-------------------------------------
-	pula_update(0, 100, x3, y3, 0, 50)
-	pula_update(0, 100, x3, y3, 0, 50)
-	pula_update(0, 100, x3, y3, 0, 50)
-	pula_update(0, 100, x3, y3, 0, 50)
-	pula_update(0, 100, x3, y3, 0, 50)
-	pula_update(0, 100, x3, y3, 0, 50)
+	pula_update(0, 100, x3, y3, 0, 50,1)
+	pula_update(0, 100, x3, y3, 0, 50,1)
+	pula_update(0, 100, x3, y3, 0, 50,1)
+	pula_update(0, 100, x3, y3, 0, 50,1)
+	pula_update(0, 100, x3, y3, 0, 50,1)
+	pula_update(0, 100, x3, y3, 0, 50,1)
 	-------------------------------------
-	pula_update(0, -100, x3, y3, 0, -25)
-	pula_update(0, -100, x3, y3, 0, -25)
-	pula_update(0, -100, x3, y3, 0, -25)
-	pula_update(0, -100, x3, y3, 0, -25)
-	pula_update(0, -100, x3, y3, 0, -25)
-	pula_update(0, -100, x3, y3, 0, -25)
+	pula_update(0, -100, x3, y3, 0, -25,1)
+	pula_update(0, -100, x3, y3, 0, -25,1)
+	pula_update(0, -100, x3, y3, 0, -25,1)
+	pula_update(0, -100, x3, y3, 0, -25,1)
+	pula_update(0, -100, x3, y3, 0, -25,1)
+	pula_update(0, -100, x3, y3, 0, -25,1)
 	-------------------------------------
+	loser_update(1, 500, 500)
 end
 
 function love.draw()
 	love.graphics.setColor(255, 255, 255)
 	love.graphics.draw(player, x, y, r, z, z, 180, 180)
-	love.graphics.draw(loser11, x3, y3, r3, z3, z3, 640, 135)
 	ship:draw("fill")
 	pula_draw()
 	pula_draw()
@@ -241,4 +283,5 @@ function love.draw()
 	pula_draw()
 	pula_draw()
 	pula_draw()
+	loser_draw()
 end
